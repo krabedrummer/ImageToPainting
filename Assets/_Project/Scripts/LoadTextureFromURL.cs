0dd28707aae78c6089d5b09c3d565b7cf739670a
krabedrummer <jayepmoore@gmail.com>
2021-07-25 11:55:03 -0400

    code comments added

--- Assets/_Project/Scripts/LoadTextureFromURL.cs
+++ Assets/_Project/Scripts/LoadTextureFromURL.cs
@@ -10,27 +10,26 @@ namespace OilPaint_JayeMoore
     {
         
         float value; //slider value
-        // float prevValue;
-        [SerializeField] private Renderer meshRenderer;
-        [SerializeField] private Material sharedMaterial;
-        [SerializeField] private GameObject texturedObject;
-        [SerializeField] private InputField imageURL;
         
-        [SerializeField] private Slider paintSlider;
+        [SerializeField] private Renderer meshRenderer; //mesh we want to change material on
+        [SerializeField] private Material sharedMaterial; // material on gameObject to be manipulated
+        [SerializeField] private GameObject texturedObject; // Quad is used for an image plain
+        [SerializeField] private InputField imageURL; // input field for URL of img
+        [SerializeField] private Slider paintSlider; //slider used to control number of paint strokes
         
-        private string TextureURL;
+        private string TextureURL; //string of img URL
     
         
         void Update()
         {
-            TextureURL = imageURL.text;
+            TextureURL = imageURL.text; // get URL from text in input field
            
-            texturedObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Radius", value);
-            value = paintSlider.value;
-            Debug.Log(value);
+            texturedObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Radius", value);// get the renderer of the Quad and get the material and its _Radius property value
+            value = paintSlider.value; // set value to slider value
+           // Debug.Log(value);
         }
         
-
+        //Start coroutine to download img from TextureURL string
         public void LoadImage()
         {
             StartCoroutine(DownloadImage(TextureURL));
@@ -40,12 +39,12 @@ namespace OilPaint_JayeMoore
         
         public void Reset()
         {
-            gameObject.GetComponent<Renderer>().material.mainTexture = null;
+            gameObject.GetComponent<Renderer>().material.mainTexture = null; // clear image
             Debug.Log("Image Reset");
         }
         IEnumerator DownloadImage(string TextureURL)
         {
-            UnityWebRequest request = UnityWebRequestTexture.GetTexture(TextureURL);
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(TextureURL);  // web request for URL
             yield return request.SendWebRequest();
             
             if (request.result != UnityWebRequest.Result.Success)
@@ -53,7 +52,7 @@ namespace OilPaint_JayeMoore
                 Debug.Log(request.error);
             }
             else
-                this.gameObject.GetComponent<Renderer>().material.mainTexture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                this.gameObject.GetComponent<Renderer>().material.mainTexture = ((DownloadHandlerTexture)request.downloadHandler).texture; //download requested imaged
         }
     } 
 }
